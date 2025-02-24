202d760c5c8cb634b5b032862fd5fd875912ec32
Aethrexal <contact@aethrexal.dev>
2025-02-24 19:58:39 +0100

    dev command added, able to install and remove rustup

--- src/modules/dev.rs
+++ src/modules/dev.rs
@@ -1,6 +1,7 @@
 use crate::args::DevArgs;
 use colored::Colorize;
 use std::process::Command;
+use which::which;
 
 pub fn dev(args: &DevArgs) {
     if args.install && args.remove {
@@ -16,7 +17,13 @@ pub fn dev(args: &DevArgs) {
     match args.option.as_str() {
         "rustup" => {
             if args.install {
-                println!("{}", "Installing rustup...".yellow());
+                if is_cmd_installed("rustup") {
+                    println!(
+                        "{}",
+                        "Rustup is already installed, nothing todo.".bold().yellow()
+                    );
+                    return;
+                }
 
                 Command::new("sh")
                     .args([
@@ -25,18 +32,39 @@ pub fn dev(args: &DevArgs) {
                     ])
                     .status()
                     .expect("Was unable to install rustup");
+
+                println!(
+                    "{}",
+                    "Rustup got installed, please restart the terminal to start using it."
+                        .italic()
+                        .yellow()
+                );
             } else if args.remove {
+                if !is_cmd_installed("rustup") {
+                    println!(
+                        "{}",
+                        "Rustup is not installed, nothing todo.".bold().yellow()
+                    );
+                    return;
+                }
+
                 Command::new("rustup")
                     .args(["self", "uninstall"])
                     .status()
                     .expect("Wasn't able to uninstall rustup");
-            } else {
-                let res = Command::new("command")
-                    .args(["-v", "rupstup", "2>&1 >/dev/null"])
-                    .output()
-                    .expect("Wasn't able to check if command exists");
 
-                println!("{:?}", res.stdout);
+                println!(
+                    "{}",
+                    "Rustup got removed, please restart the terminal to start using it."
+                        .italic()
+                        .yellow()
+                );
+            } else {
+                if is_cmd_installed("rustup") {
+                    println!("Rustup is currently: {}", "installed".bold().green());
+                } else {
+                    println!("Rustup is currently: {}", "not installed".bold().red());
+                }
             }
         }
 
@@ -49,4 +77,15 @@ pub fn dev(args: &DevArgs) {
             return;
         }
     }
+}
+
+fn is_cmd_installed(cmd: &str) -> bool {
+    match which(cmd) {
+        Ok(_) => {
+            return true;
+        }
+        Err(_) => {
+            return false;
+        }
+    };
 }
