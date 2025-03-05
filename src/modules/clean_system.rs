ea8e729c9b64f5d0fa46a0d0a8ec7961ea4723e1
Luna <contact@aethrexal.dev>
2025-03-05 18:40:24 +0100

    0.1.1-Dev using own util for done in clenaup

--- src/modules/clean_system.rs
+++ src/modules/clean_system.rs
@@ -1,17 +1,31 @@
 use std::process::Command;
-use colored::Colorize;
+
+use crate::utils::status_msg::done;
 
 pub fn clean_system() {
-  run_cmd("podman", &["image", "prune", "-af"], "Failed to prune podman images");
-  run_cmd("podman", &["volume", "prune", "-f"], "Failed to prune podman volumes");
-  run_cmd("flatpak", &["uninstall", "--unused"], "Failed to uninstall unused flatpaks");
-  run_cmd("rpm-ostree", &["cleanup", "-bm"], "Failed to cleanup rpm-ostree");
+    run_cmd(
+        "podman",
+        &["image", "prune", "-af"],
+        "Failed to prune podman images",
+    );
+    run_cmd(
+        "podman",
+        &["volume", "prune", "-f"],
+        "Failed to prune podman volumes",
+    );
+    run_cmd(
+        "flatpak",
+        &["uninstall", "--unused"],
+        "Failed to uninstall unused flatpaks",
+    );
+    run_cmd(
+        "rpm-ostree",
+        &["cleanup", "-bm"],
+        "Failed to cleanup rpm-ostree",
+    );
 }
 
 fn run_cmd(cmd: &str, args: &[&str], msg: &str) {
-  Command::new(&cmd)
-    .args(&*args)
-    .spawn()
-    .expect(&msg);
-  println!("{}: {}", &cmd, "Done".bold().green());
+    Command::new(&cmd).args(&*args).spawn().expect(&msg);
+    done(format!("{}", &cmd).as_str());
 }
